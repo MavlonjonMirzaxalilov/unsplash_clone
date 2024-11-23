79321261cf67889b2fc83323186ecafb4d849e51
MavlonjonMirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-11-23 13:19:17 +0500

    register and login page design ready

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Form, Link } from "react-router-dom";
+import { FormInput } from "../components";
 
 function Login() {
-	return (
-		<div>Login</div>
-	)
+  return (
+    <div className="flex min-h-screen w-full">
+      <div className="auth-bg hidden w-[40%] md:block"></div>
+      <div className="auth-bg flex w-full items-center justify-center md:w-[60%] md:bg-none">
+        <Form method="post" className="w-full max-w-96 px-5 md:px-0">
+          <h1 className="mb-5 text-center font-display text-3xl font-semibold text-white md:text-4xl md:text-black">
+            Login
+          </h1>
+          <div className="flex flex-col gap-2 md:gap-5">
+            <FormInput placeholder="Email" name="email" type="email" />
+            <FormInput placeholder="Password" name="password" type="password" />
+          </div>
+
+          <div className="my-5 flex flex-col gap-3 md:mt-10 md:flex-row md:gap-5">
+            <button
+              type="submit"
+              className="btn btn-primary btn-sm grow font-display text-xl md:btn-md hover:text-white"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary btn-sm grow font-display text-xl md:btn-md hover:text-white"
+            >
+              <span>Google</span>
+              <FcGoogle />
+            </button>
+          </div>
+          <div className="flex justify-between">
+            <Link className="text-white md:text-black">Forgot password ? </Link>
+            <Link
+              to={"/register"}
+              className="link link-primary text-white md:text-black"
+            >
+              You don't have account yet?
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Form } from "react-router-dom";
-import { FormInput } from "../components";
 import { FcGoogle } from "react-icons/fc";
+import { Form, Link } from "react-router-dom";
+import { FormInput } from "../components";
 
 function Register() {
   return (
     <div className="flex min-h-screen w-full">
-      <div className='w-[40%] bg-orange-300 bg-[url("https://picsum.photos/900/1200")] bg-cover bg-center'></div>
-      <div className="flex w-[60%] items-center justify-center">
-        <Form method="post" className="w-full max-w-96">
-          <h1 className="mb-5 text-center font-display text-4xl font-semibold">
+      <div className="auth-bg hidden w-[40%] md:block"></div>
+      <div className="auth-bg flex w-full items-center justify-center md:w-[60%] md:bg-none">
+        <Form method="post" className="w-full max-w-96 px-5 md:px-0">
+          <h1 className="mb-5 text-center font-display text-3xl font-semibold text-white md:text-4xl md:text-black">
             Register
           </h1>
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-2 md:gap-5">
             <FormInput placeholder="Full Name" name="displayName" type="text" />
             <FormInput placeholder="Email" name="email" type="email" />
             <FormInput placeholder="Password" name="password" type="password" />
@@ -22,20 +22,28 @@ function Register() {
               type="password"
             />
           </div>
-          <div className="my-10 flex gap-5">
+          <div className="my-5 flex flex-col gap-3 md:my-10 md:flex-row md:gap-5">
             <button
               type="submit"
-              className="btn btn-primary grow font-display text-xl hover:text-white"
+              className="btn btn-primary btn-sm grow font-display text-xl md:btn-md hover:text-white"
             >
               Register
             </button>
             <button
               type="button"
-              className="btn btn-secondary grow font-display text-xl hover:text-white"
+              className="btn btn-secondary btn-sm grow font-display text-xl md:btn-md hover:text-white"
             >
               <span>Google</span>
               <FcGoogle />
             </button>
+          </div>
+          <div className="text-center">
+            <Link
+              to={"/login"}
+              className="link link-primary text-white md:text-black"
+            >
+              You already have account ?
+            </Link>
           </div>
         </Form>
       </div>
